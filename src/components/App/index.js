8655ab30e8260d89823af28c871438e640115c31
DamianStrz <damian.strzalkowski@o2.pl>
2021-01-22 00:06:00 +0100

    Changing component Navigation to display differently for authenticated and nonauthenticatedu user. It requaierd checking changes in authenticated user object in firebase and using this changes in state of the App component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
     HashRouter as Router,
     Route,
@@ -14,25 +14,67 @@ import AccountPage from "../Account";
 import AdminPage from "../Admin";
 import PasswordForgetPage from "../PasswordForget";
 
+import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
-const App = () => (
-    <Router>
-        <Navigation/>
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-        <hr/>
+        this.state = {
+            authUser:null
+        }
+    }
 
-        <Switch>
-            <Route exact path={ROUTES.LANDING} component={LandingPage}/>
-            <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
-            <Route path={ROUTES.PRIORITIES} component={PrioritiesPage}/>
-            <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
-            <Route path={ROUTES.ADMIN} component={AdminPage}/>
-            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
-        </Switch>
+    /*
+    When the component is mounted in DOM the listener function starts the
+    helping method from auth object in firebase. Its parameter is another function
+    with authUser object. This function is called every time something changes in
+    authenticated user - signing in, signing up, signing out. For example if user
+    signs out the authUser object will become null.
 
-    </Router>
-)
+    The state of app component will be change to null and based on that some
+    components can adjust their behaviour depending on that (like Navigation
+    component)
+    */
 
-export default App;
+    componentDidMount() {
+        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser
+                ? this.setState({ authUser })
+                : this.setState({ authUser:null })
+        });
+    }
+
+
+    //When component unmounts the listener is removed;
+
+    componentWillUnmount() {
+        this.listener();
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navigation authUser={this.state.authUser}/>
+
+                <hr/>
+
+                <Switch>
+                    <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+                    <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+                    <Route path={ROUTES.PRIORITIES} component={PrioritiesPage}/>
+                    <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
+                    <Route path={ROUTES.ADMIN} component={AdminPage}/>
+                    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+
+
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,30 +4,49 @@ import { Link } from "react-router-dom";
 import SignOutButton from "../SignOut";
 import * as ROUTES from "../../constants/routes";
 
+/*
+* Component Navigation has two versions based on authUser value which is the
+* state of the App component. If authUser is null component Navigation will render
+* different version then when authUser will be true.
+*
+* */
 
-const Navigation = () => (
+const Navigation = ({authUser}) => (
     <div>
-        <ul>
-            <li>
-                <Link to={ROUTES.LANDING}>Landing page</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.PRIORITIES}>Your priorities</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.ACCOUNT}>Account</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.ADMIN}>Admin</Link>
-            </li>
-            <li>
-                <SignOutButton/>
-            </li>
-        </ul>
+        {authUser ? <NavigationAuth/> : <NavigationNonAuth/>}
     </div>
 )
+
+const NavigationAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing page</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.PRIORITIES}>Your priorities</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </li>
+        <li>
+            <SignOutButton/>
+        </li>
+    </ul>
+)
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing page</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+    </ul>
+)
+
+
+
+
 
 export default Navigation;
