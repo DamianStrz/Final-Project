21ec6901715deb76b244d302a3288b42d2c49a87
DamianStrz <damian.strzalkowski@o2.pl>
2021-01-22 12:23:37 +0100

    Account component implemented. Account page contains PasswordForgetForm and PasswordChangeForm accessible for user.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+
 const AccountPage = () => (
     <div>
-        <h1>Account component</h1>
+        <h1>Account Page. What do you need?</h1>
+        <h2>Password reset?</h2>
+        <PasswordForgetForm/>
+        <h2>Password change?</h2>
+        <PasswordChangeForm/>
     </div>
 )
 
